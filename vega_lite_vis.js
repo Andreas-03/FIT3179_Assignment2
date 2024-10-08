592f28250ab71f807a7369f1c8a910a7abab6beb
Andreas-03 <akok0011@student.monash.edu>
2024-10-09 07:33:28 +0800

    Fixed issues and modified HTML

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,21 @@
-var vg_1 = "map_schema.json"
-var vg_2 = "line_plot_schema.json"
+var vg_1 = "bubble_plot_schema.json";
+var vg_2 = "polar_area_chart_schema.json";
+var vg_3 = "map_schema.json";
+var vg_4 = "line_plot_schema.json";
 
-vegaEmbed("#map", vg_1).then(function(result) {
+
+vegaEmbed("#bubble_chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#line_plot", vg_2).then(function(result) {
+vegaEmbed("#polar_area_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#map", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#line_plot", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
