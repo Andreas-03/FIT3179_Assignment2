42205731d5a4dff42e2f7da14a1d94fe3d41f4b1
Andreas-03 <akok0011@student.monash.edu>
2024-10-11 01:45:53 +0800

    Created group bar chart for prevalence of HIV in risk groups

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,7 @@ var vg_1 = "bubble_plot_schema.json";
 var vg_2 = "polar_area_chart_schema.json";
 var vg_3 = "map_schema.json";
 var vg_4 = "line_plot_schema.json";
+var vg_5 = "grouped_bar_chart_schema.json";
 
 
 vegaEmbed("#bubble_chart", vg_1).then(function(result) {
@@ -17,5 +18,9 @@ vegaEmbed("#map", vg_3).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#line_plot", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#grouped_bar_chart", vg_5).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
