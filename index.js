13ef947cf9390515a06741248f9c60e18c2c9538
Andreas-03 <akok0011@student.monash.edu>
2024-10-15 20:47:48 +0800

    separate css and js from html

---
+++ index.js
@@ -0,0 +1,13 @@
+var vg_1 = "bubble_plot_schema.json";
+var vg_2 = "polar_area_chart_schema.json";
+var vg_3 = "map_schema.json";
+var vg_4 = "line_plot_schema.json";
+var vg_5 = "grouped_bar_chart_schema.json";
+var vg_6 = "box_plot_schema.json";
+
+vegaEmbed("#bubble_chart", vg_1).catch(console.error);
+vegaEmbed("#polar_area_chart", vg_2).catch(console.error);
+vegaEmbed("#map", vg_3).catch(console.error);
+vegaEmbed("#line_plot", vg_4).catch(console.error);
+vegaEmbed("#grouped_bar_chart", vg_5).catch(console.error);
+vegaEmbed("#box_plot", vg_6).catch(console.error);
